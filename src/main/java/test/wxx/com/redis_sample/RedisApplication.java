b320f08f59cfbbf6d9870a0297f05f01f26baf5e
tcwangxiaoxi <tcwangxiaoxi@gmail.com>
2016-05-26 18:42:35 +0800

    添加pom即测试类

---
+++ src/main/java/test/wxx/com/redis_sample/RedisApplication.java
@@ -0,0 +1,11 @@
+package test.wxx.com.redis_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(RedisApplication.class, args);
+	}
+}
